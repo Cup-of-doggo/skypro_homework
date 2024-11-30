9fda82612842d01f1f1b2d492d39a58c4bce4717
Gordey <gordej.taran@mail.ru>
2024-12-01 00:07:13 +0500

    'flake8 fixes'

---
+++ decorators.py
@@ -0,0 +1,28 @@
+import runpy
+from time import strftime
+from functools import wraps
+
+
+
+
+
+
+def log(func):
+    #def inner(func):
+        @wraps(func)
+        def wrapper(arg):
+            try:
+                time_1 = strftime('%H:%M:%S')
+                print(f'Начало работы фунции {time_1}')
+                result = func(arg)
+                time_2 = strftime('%H:%M:%S')
+                print(f'Конец работы функции {time_2}')
+                print(f'{func.__name__} все ок \nРезультат: {result}')
+                return result
+            except Exception as err:
+                return print(f'{func.__name__} Ошибка {err}\nВходные параметры {arg}')
+        return wrapper
+    #return inner
+
+
+
