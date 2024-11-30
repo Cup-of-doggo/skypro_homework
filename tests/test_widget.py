e0267faa20b0086c010a14ee1876b9f65a9b6495
Gordey <gordej.taran@mail.ru>
2024-11-30 20:47:46 +0500

    'flake8 fixes'

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,5 +1,5 @@
-from src.widget import get_date
-from src.widget import mask_account_card
+from src.widget import get_date, mask_account_card
+
 
 def test_mask_acc_card():
     assert mask_account_card('Visa Gold 5999414228426353') == 'Visa Gold 5999 41** **** 6353'
