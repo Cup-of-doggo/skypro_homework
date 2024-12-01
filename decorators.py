a7c77ec1fa3f2d6fbbaffd7057741ce9ee2ceb29
Gordey <gordej.taran@mail.ru>
2024-12-01 18:00:15 +0500

    homework fix

--- decorators.py
+++ decorators.py
@@ -4,21 +4,16 @@ from functools import wraps
 
 
 def log(func):
-    #def inner(func):
-        @wraps(func)
-        def wrapper(arg):
-            try:
-                time_1 = strftime('%H:%M:%S')
-                print(f'Начало работы фунции {time_1}')
-                result = func(arg)
-                time_2 = strftime('%H:%M:%S')
-                print(f'Конец работы функции {time_2}')
-                print(f'{func.__name__} все ок \nРезультат: {result}')
-                return result
-            except Exception as err:
-                return print(f'{func.__name__} Ошибка {err}\nВходные параметры {arg}')
-        return wrapper
-    #return inner
-
-
-
+    @wraps(func)
+    def wrapper(arg):
+        try:
+            time_1 = strftime('%H:%M:%S')
+            print(f'Начало работы фунции {time_1}')
+            result = func(arg)
+            time_2 = strftime('%H:%M:%S')
+            print(f'Конец работы функции {time_2}')
+            print(f'{func.__name__} все ок \nРезультат: {result}')
+            return result
+        except Exception as err:
+            return print(f'{func.__name__} Ошибка {err}\nВходные параметры {arg}')
+    return wrapper
